c7b9f779f70d9db1c279d69540a9c466e5936776
DAiello02 <92293844+DAiello02@users.noreply.github.com>
2024-10-07 13:30:52 +0200

    Add files via upload

---
+++ segreteria/cmake-build-debug/CMakeFiles/segreteria.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/segreteria.dir/main.c.o"
+  "CMakeFiles/segreteria.dir/main.c.o.d"
+  "segreteria"
+  "segreteria.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/segreteria.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ studente/cmake-build-debug/CMakeFiles/studente.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/studente.dir/main.c.o"
+  "CMakeFiles/studente.dir/main.c.o.d"
+  "studente"
+  "studente.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/studente.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
